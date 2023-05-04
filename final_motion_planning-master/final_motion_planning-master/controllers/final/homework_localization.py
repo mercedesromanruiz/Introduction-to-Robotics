83736e90779a4de1542a0d2285bad5fdf25be85b
Mercedes Roman Ruiz <122405877+mercedesromanruiz@users.noreply.github.com>
2023-05-03 20:27:04 -0500

    Update homework_localization.py
    Maximum likelihood estimator working :)

--- final_motion_planning-master/final_motion_planning-master/controllers/final/homework_localization.py
+++ final_motion_planning-master/final_motion_planning-master/controllers/final/homework_localization.py
@@ -28,7 +28,7 @@ def log_likelihood_single(expected, observed):
     #    Fill out the underscore parts.
     #    Assume that observation error is known to follow N(0, sigma^2)
     #    See https://en.wikipedia.org/wiki/Normal_distribution#Operations_on_a_single_normal_variable for the equation.
-    return math.log(1 / (sigma * math.sqrt(2 * math.pi))) - 1/2 * ((mu - x) / sigma)**2
+    return math.log(1 / (sigma * math.sqrt(2 * math.pi))) - (-1/2 * (((mu - x) / sigma)**(2)))
     
 # 2. Calculate the log-likelihood of the given estimate and observations
 #    estimate: [x, y, th]
